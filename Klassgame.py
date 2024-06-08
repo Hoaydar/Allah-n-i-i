e479a49a89150430330ee87528412340ffc3e07a
Haydar <133051958+Hoaydar@users.noreply.github.com>
2024-06-08 20:21:59 +0300

    Update Klassgame.py

--- Klassgame.py
+++ Klassgame.py
@@ -3,30 +3,34 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import telegram
-import time
+from aiogram import Bot, Dispatcher, types
 import asyncio
+import time
 
 url = "https://www.klasgame.com/mmorpg-oyunlar/knight-unity/knight-unity-goldbar"
 my_token = '6463031187:AAGAVe5K6yWqH9vTSz5sGLGL2LKWmEodzjw'
 my_chat_id = -1002209424495
 
-def send(msg, chat_id, token=my_token):
-    bot = telegram.Bot(token=token)
-    bot.sendMessage(chat_id=chat_id, text=msg)
+async def send(msg, chat_id, token=my_token):
+    bot = Bot(token=token)
+    await bot.send_message(chat_id=chat_id, text=msg)
 
-while True:
-    driver = webdriver.Chrome()
-    driver.get(url)
+async def main():
+    while True:
+        driver = webdriver.Chrome()
+        driver.get(url)
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    xpath = driver.find_element(By.XPATH, "//*[@id='urunler']/div[4]/div[1]/div[3]/div[2]/div[2]/div/div[1]/div[5]/a")
-    data = xpath.get_attribute('href')
-    onclick = xpath.get_attribute('onclick')
-    if onclick == "message('Şu an için alış aktif görünmüyor, lütfen daha sonra tekrar deneyiniz.', 'danger'); return false;":
-        print('devam etmeli')
-    else:
-        asyncio.run(send(msg='data', chat_id=my_chat_id, token=my_token))
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        xpath = driver.find_element(By.XPATH, "//*[@id='urunler']/div[4]/div[1]/div[3]/div[2]/div[2]/div/div[1]/div[5]/a")
+        data = xpath.get_attribute('href')
+        onclick = xpath.get_attribute('onclick')
+        if onclick == "message('Şu an için alış aktif görünmüyor, lütfen daha sonra tekrar deneyiniz.', 'danger'); return false;":
+            print('devam etmeli')
+        else:
+            await send(msg='data', chat_id=my_chat_id, token=my_token)
 
-    driver.quit()
-    time.sleep(5)
+        driver.quit()
+        await asyncio.sleep(5)
+
+if __name__ == "__main__":
+    asyncio.run(main())
