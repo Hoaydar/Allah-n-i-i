526dc4b8ced77e9621f1c5bc4e427a94b5a784e2
Haydar <133051958+Hoaydar@users.noreply.github.com>
2024-06-08 10:10:32 +0300

    Update Klassgame.py

--- Klassgame.py
+++ Klassgame.py
@@ -1,16 +1,32 @@
-import undetected_chromedriver as webdriver
- 
-# Create an instance of the undetected ChromeDriver in headless mode
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import telegram
+import time
 
-driver = webdriver.Chrome(options=options)
- 
-# Navigate to target website
-driver.get("https://www.g2.com/products/asana/reviews")
- 
-# Take a screenshot
-driver.save_screenshot("screenshot.png")
- 
-# Close the browser
-driver.quit()
+url = "https://www.klasgame.com/mmorpg-oyunlar/knight-unity/knight-unity-goldbar"
+my_token = '6463031187:AAGAVe5K6yWqH9vTSz5sGLGL2LKWmEodzjw'
+my_chat_id = -1002209424495
+
+def send(msg, chat_id, token=my_token):
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=chat_id, text=msg)
+
+while True:
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    a_tag = soup.find('a', class_='product-sell')
+
+    # 'a' etiketinin href özelliğini alarak kontrol etme
+    if a_tag.get('onclick') != "message('Şu an için alış aktif görünmüyor, lütfen daha sonra tekrar deneyiniz.', 'danger'); return false;":
+        send(msg=a_tag['href'], chat_id=my_chat_id, token=my_token)
+    else:
+        print("Href değeri 'false' veya 'a' etiketi bulunamadı.")
+
+    driver.quit()
+    time.sleep(5)
