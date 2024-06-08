cef7a05531dc352e39ffed9b51340db2e9b33588
Haydar <133051958+Hoaydar@users.noreply.github.com>
2024-06-08 13:56:48 +0300

    Update Klassgame.py

--- Klassgame.py
+++ Klassgame.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 import telegram
 import time
 
@@ -15,27 +14,20 @@ def send(msg, chat_id, token=my_token):
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=chat_id, text=msg)
 
-service = Service('/path/to/chromedriver')  # Chromedriver yolunu buraya girin
-options = Options()
-options.headless = True  # Başsız tarayıcı modunu etkinleştirir
-
 while True:
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome()
     driver.get(url)
-    
-    try:
-        # Belirli bir elementi bekle
-        wait = WebDriverWait(driver, 10)
-        xpath = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='urunler']/div[4]/div[1]/div[3]/div[2]/div[2]/div/div[1]/div[5]/a")))
-        href = xpath.get_attribute('href')
 
-        # Href kontrolü ve mesaj gönderme
-        if href:
-            send(msg=href, chat_id=my_chat_id, token=my_token)
-        else:
-            print("Href değeri 'false' veya 'a' etiketi bulunamadı.")
-    except Exception as e:
-        print(f"Hata oluştu: {e}")
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    xpath = driver.find_element(By.XPATH, "//*[@id='urunler']/div[4]/div[1]/div[3]/div[2]/div[2]/div/div[1]/div[5]/a")
+    a =xpath.click()
+    a_tag = soup.find('a', class_='product-sell')
+
+    # 'a' etiketinin href özelliğini alarak kontrol etme
+    if a.status_code == 200:
+        send(msg=a_tag['href'], chat_id=my_chat_id, token=my_token)
+    else:
+        print("Href değeri 'false' veya 'a' etiketi bulunamadı.")
 
     driver.quit()
     time.sleep(5)
